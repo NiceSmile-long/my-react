71e6e083cc9deeea079281aeee3eb889af170423
e <337623361@qq.com>
2020-04-27 21:47:17 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import MyButton from './components/Button/Button'
+import {Button,DatePicker} from 'antd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const { RangePicker } = DatePicker;
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <MyButton>杨幂</MyButton>
+        <Button type="primary">杨幂</Button>
+        <RangePicker />
+      </div>
+    )
+  }
+}  
